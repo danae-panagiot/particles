fca82e089e3c93394ea68dad4dbbc10cb946910f
danae-panagiot <danae.panagiotopoulou@lego.com>
2024-04-10 12:10:16 +0200

    chore: making a brick out of pictures

--- particles/src/components/Structure.tsx
+++ particles/src/components/Structure.tsx
@@ -1,29 +1,51 @@
 import React from "react";
+import '../Structure.css';
 import { motion } from "framer-motion";
 
 const Structure: React.FC = () => {
+  const spacingX = window.innerWidth / 4; 
+  const spacingY = window.innerHeight / 4;
   return (
     <div className="structure">
       <motion.img
         src="/images/cat.png"
-        style={{ width: 100, height: 100 }}
-        initial={{ x: -100, y: -100 }}
-        animate={{ x: 400, y: 100 }}
+        initial={{ x: -spacingX, y: spacingY }} // Initial position
+        animate={{ x: (window.innerWidth / 2 - spacingX/2), y: window.innerHeight / 4 }} // Animate to the middle
         transition={{ duration: 2 }}
       />
       <motion.img
         src="/images/hey.png"
-        style={{ width: 100, height: 100 }}
-        initial={{ x: 0, y: 0 }}
-        animate={{ x: 400, y: 200 }}
+        initial={{ x:spacingX, y: spacingY }} // Initial position
+        animate={{ x: window.innerWidth - 2*spacingX,y: window.innerHeight / 4  }} // Animate to the middle
         transition={{ duration: 2 }}
       />
       <motion.img
         src="/images/heart.png"
-        style={{ width: 100, height: 100 }}
-        initial={{ x: 100, y: 100 }}
-        animate={{ x: 400, y: 300 }}
+        initial={{ x: window.innerWidth + spacingX , y: spacingY }} // Initial position
+        animate={{ x: window.innerWidth /2 + spacingX/2, y: window.innerHeight / 4  }} // Animate to the middle
         transition={{ duration: 2 }}
+        />
+      <motion.img
+        src="/images/go.png"
+        style={{width: 200, height:200}}
+        initial={{ x: -spacingX, y: -spacingY }} // Initial position
+        animate={{ x: (window.innerWidth / 2 - spacingX/2) - 400, y: window.innerHeight / 2 }} // Animate to the middle
+        transition={{ duration: 2 }}
+      />
+      <motion.img
+        src="/images/smile.png"
+        style={{width: 200, height:200}}
+        initial={{ x:spacingX, y: -spacingY }} // Initial position
+        animate={{ x: window.innerWidth - 2*spacingX - 100 , y: window.innerHeight / 2  }} // Animate to the middle
+        transition={{ duration: 2 }}
+      />
+      <motion.img
+        src="/images/hi.png"
+        style={{width: 200, height:200}}
+        initial={{ x: window.innerWidth + spacingX  , y: -spacingY }} // Initial position
+        animate={{ x: window.innerWidth /2 + spacingX/2 + 200, y: window.innerHeight / 2  }} // Animate to the middle
+        transition={{ duration: 2 }}
+
       />
     </div>
   );
